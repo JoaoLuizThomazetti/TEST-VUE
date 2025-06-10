1ed8fe616b2244e02553659f868df0dd8a24eadd
João <joao@offertech.com.br>
2025-06-10 17:41:14 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import customCard from './components/customCard.vue';
+
+const products = Array.from({ length: 5 }, (_, i) => ({
+  name: `Product ${i + 1}`,
+  code: `P00${i + 1}`
+}));
+
+</script>
+
+<template>
+  <header>Testing VUE</header>
+  <customCard v-for="(product, index) in products" :key="index" :product="product" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/customCard.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="customCard">
+        <!-- <h1>Custom Card Component</h1> -->
+        <div class="cardInfo">
+            <h2>Name: {{ product.name }}</h2>
+            <h2>Code: #{{ product.code }}</h2>
+            <h2>Stock: {{ stock }}</h2>
+            <div class="stockControls">
+                <button @click="raiseStock">+</button>
+                <button @click="lowerStock">-</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    const props = defineProps({
+        product: {
+            type: Object,
+            required: true
+        }
+    });
+    // import { reactive } from 'vue';
+    // const product = reactive({
+    //     name: 'Custom Product',
+    //     code: 'CP001',
+    //     stock: 0
+    // });
+    // function raiseStock() {
+    //     product.stock++;
+    // }
+    // function lowerStock() {
+    //     if (product.stock > 0) {
+    //         product.stock--;
+    //     }
+    // }
+    import { ref } from 'vue';
+    const stock = ref(0);
+    function raiseStock() {
+        stock.value++;
+    }
+    function lowerStock() {
+        if (stock.value > 0) {
+            stock.value--;
+        }
+    }
+</script>
+
+<style scoped>
+.customCard {
+    background-color: white;
+    padding: 15px;
+    margin: 15px;
+    border-radius: 6px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+    text-align: center;
+    color: black;
+}
+
+.customCard:hover {
+    transform: scale(1.04);
+    z-index: 10;
+    box-shadow: 0 8px 20px rgba(0, 155, 182, 0.5);
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.cardInfo {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 14px;
+}
+.stockControls {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    margin-left: -200px;
+}
+</style>
